7c943fa2e91dd021e2cffb7d8ca6d4fd33235e1d
anorudes <anorudes@gmail.com>
2019-03-27 19:19:58 +0500

    Update public url

---
+++ precache-manifest.a01911ba5d3bb95215fc81d8867f6f4d.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f0a7e9000ab164f81822e03f05004dd",
+    "url": "/galleon/static/media/g.7f0a7e90.png"
+  },
+  {
+    "revision": "99a54b47766bf4a81a38",
+    "url": "/galleon/static/css/main.7921a537.chunk.css"
+  },
+  {
+    "revision": "e3bdae60cdee24b368f1",
+    "url": "/galleon/static/js/runtime~main.e48db09e.js"
+  },
+  {
+    "revision": "6d71e846a1bed52b5d86",
+    "url": "/galleon/static/js/2.7af585d4.chunk.js"
+  },
+  {
+    "revision": "40e6e8e1bd77be89d76be0ff1f124eb7",
+    "url": "/galleon/static/media/bg-2.40e6e8e1.png"
+  },
+  {
+    "revision": "f6c3cbcb874b8f5d016862b83b79090d",
+    "url": "/galleon/static/media/bg-1.f6c3cbcb.png"
+  },
+  {
+    "revision": "a88298894f8de6c3c0122c0923187047",
+    "url": "/galleon/static/media/bg-2.a8829889.png"
+  },
+  {
+    "revision": "6548c9cb734c705930195e99a12f27b1",
+    "url": "/galleon/static/media/bg-1.6548c9cb.png"
+  },
+  {
+    "revision": "32bbbe49bd3ccef96de65fbf296fb705",
+    "url": "/galleon/static/media/bg-2.32bbbe49.png"
+  },
+  {
+    "revision": "604bc2b8b2f6fa6e2d030d3a82836528",
+    "url": "/galleon/static/media/bg-1.604bc2b8.png"
+  },
+  {
+    "revision": "c6bd2a2e76f561e29a155b364eeacbf3",
+    "url": "/galleon/static/media/bg-2.c6bd2a2e.png"
+  },
+  {
+    "revision": "e868149fafac67c53b6d606338882836",
+    "url": "/galleon/static/media/bg-1.e868149f.png"
+  },
+  {
+    "revision": "185df6857c8dcd3d124d0cfa7654b334",
+    "url": "/galleon/static/media/bg-2.185df685.png"
+  },
+  {
+    "revision": "e2cf43fb126f5b5e41de61ef90ef9526",
+    "url": "/galleon/static/media/bg-1.e2cf43fb.png"
+  },
+  {
+    "revision": "99a54b47766bf4a81a38",
+    "url": "/galleon/static/js/main.844bcd98.chunk.js"
+  },
+  {
+    "revision": "98f868da93a56014b5c3668e7218542d",
+    "url": "/galleon/static/media/c.98f868da.png"
+  },
+  {
+    "revision": "a25238a7093e773a137bff8f94610e6b",
+    "url": "/galleon/static/media/gradient.a25238a7.jpg"
+  },
+  {
+    "revision": "3515f27ef128c3cbc6e0b44b2dd68964",
+    "url": "/galleon/static/media/NocturneSerif-Light.3515f27e.woff2"
+  },
+  {
+    "revision": "eb3bfe86b027b2e75eb65936bca760e1",
+    "url": "/galleon/static/media/NocturneSerif-Lightitalic.eb3bfe86.woff2"
+  },
+  {
+    "revision": "50801978eb5256bc954dbe8e5246c616",
+    "url": "/galleon/static/media/NocturneSerif-Lightitalic.50801978.woff"
+  },
+  {
+    "revision": "69152a8e880c972257c4c8c5b67e7816",
+    "url": "/galleon/static/media/NocturneSerif-Regular.69152a8e.woff2"
+  },
+  {
+    "revision": "9ef3466cddccdedf805c5a54e0a098ea",
+    "url": "/galleon/static/media/NocturneSerif-Light.9ef3466c.woff"
+  },
+  {
+    "revision": "865bbd8bec70bd8ff556f052311f2c03",
+    "url": "/galleon/static/media/NocturneSerif-Regular.865bbd8b.woff"
+  },
+  {
+    "revision": "771bd930178b8d03885d0a59cbe1439a",
+    "url": "/galleon/static/media/NocturneSerif-Mediumitalic.771bd930.woff2"
+  },
+  {
+    "revision": "eb1f5c0c509f0cd520da3a76cb8cf072",
+    "url": "/galleon/static/media/NocturneSerif-Mediumitalic.eb1f5c0c.woff"
+  },
+  {
+    "revision": "2d121d111a02d226e89e51b1aa1b468e",
+    "url": "/galleon/static/media/NocturneSerif-Medium.2d121d11.woff2"
+  },
+  {
+    "revision": "9c597489337c3ff4e0f0d41b2bbcdfa5",
+    "url": "/galleon/static/media/NocturneSerif-Medium.9c597489.woff"
+  },
+  {
+    "revision": "f93850676a21e9c318afdb8bdbb1b480",
+    "url": "/galleon/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af74eb88fdf64ed44299370cac14b9be.js"
+  "/galleon/precache-manifest.a01911ba5d3bb95215fc81d8867f6f4d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/galleon/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
