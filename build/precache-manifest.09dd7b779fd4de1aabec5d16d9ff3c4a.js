2d5e8cc1e5c4690a645ac11cdbc7793f46b2f8f4
anorudes <anorudes@gmail.com>
2019-04-09 18:52:20 +0500

    Build

--- build/precache-manifest.09dd7b779fd4de1aabec5d16d9ff3c4a.js
+++ build/precache-manifest.09dd7b779fd4de1aabec5d16d9ff3c4a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "9a84eddf8c11602ec5220d798257b153",
-    "url": "/galleon/static/media/390FD6_0_0.9a84eddf.eot"
+    "revision": "0956fa59d304d25b5638c465301e8b2c",
+    "url": "/galleon/static/media/gradient-mobile.0956fa59.jpg"
   },
   {
-    "revision": "947af5e869948fb47c88",
-    "url": "/galleon/static/css/main.39a42927.chunk.css"
+    "revision": "f359bb1a3eeced9757d3",
+    "url": "/galleon/static/css/main.85605684.chunk.css"
   },
   {
     "revision": "e3bdae60cdee24b368f1",
     "url": "/galleon/static/js/runtime~main.e48db09e.js"
   },
   {
-    "revision": "5b66daf2390639a9e29c",
-    "url": "/galleon/static/js/2.55ebd2ed.chunk.js"
+    "revision": "71710ecddb0a8169aa06",
+    "url": "/galleon/static/js/2.b8734c39.chunk.js"
   },
   {
     "revision": "01eb2ebedd6404fe857fbd1da489127a",
@@ -68,16 +68,20 @@ self.__precacheManifest = [
     "url": "/galleon/static/media/c.98f868da.png"
   },
   {
+    "revision": "2ffb94c3a8b2fd8a2633de76fc07dfad",
+    "url": "/galleon/static/media/logo.2ffb94c3.svg"
+  },
+  {
     "revision": "a25238a7093e773a137bff8f94610e6b",
     "url": "/galleon/static/media/gradient.a25238a7.jpg"
   },
   {
-    "revision": "0956fa59d304d25b5638c465301e8b2c",
-    "url": "/galleon/static/media/gradient-mobile.0956fa59.jpg"
+    "revision": "f359bb1a3eeced9757d3",
+    "url": "/galleon/static/js/main.f9ec9cad.chunk.js"
   },
   {
-    "revision": "947af5e869948fb47c88",
-    "url": "/galleon/static/js/main.649410ba.chunk.js"
+    "revision": "9a84eddf8c11602ec5220d798257b153",
+    "url": "/galleon/static/media/390FD6_0_0.9a84eddf.eot"
   },
   {
     "revision": "e95d4986339f94723be4fe5047744723",
@@ -88,28 +92,32 @@ self.__precacheManifest = [
     "url": "/galleon/static/media/390FD6_0_0.4478beff.woff"
   },
   {
-    "revision": "a37d460b985283f2bcff1efe68e17eaa",
-    "url": "/galleon/static/media/390FD6_0_0.a37d460b.ttf"
+    "revision": "e5e03e6deca5cd3954734f9c1696ed2d",
+    "url": "/galleon/static/media/390FD6_3_0.e5e03e6d.eot"
   },
   {
-    "revision": "9ef3466cddccdedf805c5a54e0a098ea",
-    "url": "/galleon/static/media/NocturneSerif-Light.9ef3466c.woff"
+    "revision": "a37d460b985283f2bcff1efe68e17eaa",
+    "url": "/galleon/static/media/390FD6_0_0.a37d460b.ttf"
   },
   {
     "revision": "3515f27ef128c3cbc6e0b44b2dd68964",
     "url": "/galleon/static/media/NocturneSerif-Light.3515f27e.woff2"
   },
   {
+    "revision": "9ef3466cddccdedf805c5a54e0a098ea",
+    "url": "/galleon/static/media/NocturneSerif-Light.9ef3466c.woff"
+  },
+  {
     "revision": "3f2485f2ee098466eeb1bc042455796d",
     "url": "/galleon/static/media/390FD6_3_0.3f2485f2.woff2"
   },
   {
-    "revision": "e5e03e6deca5cd3954734f9c1696ed2d",
-    "url": "/galleon/static/media/390FD6_3_0.e5e03e6d.eot"
-  },
-  {
     "revision": "7c9712413c163a6f968d4d60b2e25b21",
     "url": "/galleon/static/media/390FD6_3_0.7c971241.woff"
+  },
+  {
+    "revision": "c37d4466fe0ac9a6c658633761014120",
+    "url": "/galleon/static/media/390FD6_3_0.c37d4466.ttf"
   },
   {
     "revision": "a788f6ecc2bf373c486c0db59a6e1f4a",
@@ -120,20 +128,16 @@ self.__precacheManifest = [
     "url": "/galleon/static/media/390FD6_2_0.4612750a.woff2"
   },
   {
-    "revision": "c37d4466fe0ac9a6c658633761014120",
-    "url": "/galleon/static/media/390FD6_3_0.c37d4466.ttf"
-  },
-  {
-    "revision": "9049d2b2458cbbd15f50d95a3522fe8b",
-    "url": "/galleon/static/media/390FD6_2_0.9049d2b2.ttf"
-  },
-  {
     "revision": "d93f6cd9c98df2a20b45bb6030c69ab0",
     "url": "/galleon/static/media/390FD6_2_0.d93f6cd9.woff"
   },
   {
     "revision": "548142f9efdb76a88157f82b687e51a9",
     "url": "/galleon/static/media/390FD6_1_0.548142f9.eot"
+  },
+  {
+    "revision": "9049d2b2458cbbd15f50d95a3522fe8b",
+    "url": "/galleon/static/media/390FD6_2_0.9049d2b2.ttf"
   },
   {
     "revision": "3e47231284442bcf863a5a5a86f26147",
@@ -148,7 +152,7 @@ self.__precacheManifest = [
     "url": "/galleon/static/media/390FD6_1_0.60a20ab3.ttf"
   },
   {
-    "revision": "537da9aa9b0af8bee774efd8b6a94524",
+    "revision": "04e2d4e58ebb3c47d99e4b3f95ea7b99",
     "url": "/galleon/index.html"
   }
 ];
