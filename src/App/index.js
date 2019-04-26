808ca36873d2c1c359e93e3a11a0287d5c1cd917
Ivan Tilikin <tilvan@ya.ru>
2019-04-26 10:33:47 +0300

    Anchor state load

--- src/App/index.js
+++ src/App/index.js
@@ -27,7 +27,6 @@ class App extends Component {
     this.switcherImagesVisible = {};
     this.switcherImagesVisible = {};
     window.animationTimers = [];
-    window.location.hash = '';
   }
   
   componentDidMount() {
@@ -49,6 +48,7 @@ class App extends Component {
     
     setTimeout(() => {
       window.addEventListener('hashchange', this.handleChangePage);
+      this.handleChangePage();
     }, 100);
     setTimeout(() => {
       window.disableMouseWheel = false;
