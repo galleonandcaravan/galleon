fb4a5aca71a3f8feb584b0ca695508bdee1f1826
Ivan Tilikin <tilvan@ya.ru>
2023-12-28 21:44:45 +0300

    Make smallprint updates

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,11 +18,22 @@ class Footer extends Component {
         />
 
         <div className="footer-copyright">
-          Galcar Limited (t/a G&C) is a company registered in England and Wales (registered no. 11656491).
-          G&C’s Payment and Foreign Currency Exchange Services are provided by Currency Cloud Limited
-          (registered no. 06323311), whose office is at Stewardship Building, 1st Floor, 12 Steward Street,
-          London, E1 6FQ. The Currency Cloud Limited is Authorised and Regulated by the Financial Conduct Authority
-          under the Electronic Money Regulations 2011 for the issuing of electronic money. FCA registration no. 900100.
+          - For clients based in the European Economic Area, payment services for Galcar Limited (t/a G&C) are provided
+          by CurrencyCloud B.V.. Registered in the Netherlands No. 72186178. Registered Office: Nieuwezijds Voorburgwal
+          296 - 298, Mindspace Nieuwezijds Office 001 Amsterdam. CurrencyCloud B.V. is authorised by the DNB under the
+          Wet op het financieel toezicht to carry out the business of an electronic-money institution (Relation Number:
+          R142701).Б<br/>
+          - For clients based in the United States, payment services for Galcar Limited (t/a G&C) are provided by The
+          Currency Cloud Inc. which operates in partnership with Community Federal Savings Bank (CFSB) to facilitate
+          payments in all 50 states in the US. CFSB is registered with the Federal Deposit Insurance Corporation (FDIC
+          Certificate# 57129). The Currency Cloud Inc is registered with FinCEN and authorised in 39 states to transmit
+          money (MSB Registration Number: 31000206794359). Registered Office: 104 5th Avenue, 20th Floor, New York , NY
+          10011.<br/>
+          - For clients based in the United Kingdom and rest of the world, payment services for Galcar Limited (t/a G&C)
+          are provided by The Currency Cloud Limited. Registered in England and Wales No. 06323311. Registered Office:
+          Stewardship Building 1st Floor, 12 Steward Street London E1 6FQ. The Currency Cloud Limited is authorised by
+          the Financial Conduct Authority under the Electronic Money Regulations 2011 for the issuing of electronic
+          money (FRN: 900199).
         </div>
       </div>
     );
